4f9803282f7814908b38eb6a4eb9134c06543ec5
bstee615 <benjaminjsteenhoek@gmail.com>
2024-01-10 13:32:02 -0600

    Provide environment.yml to fully initialize the Conda environment

--- scripts/setup_environment.sh
+++ scripts/setup_environment.sh
@@ -1,9 +1,7 @@
-# Create virtual environment
-conda create --name deepdfa python=3.10 -y
+#!/bin/bash
+set -e
+
+conda create -f environment.yml
 conda activate deepdfa
-# Install requirements
-conda install cudatoolkit=11.8 -y
 export LD_LIBRARY_PATH="$CONDA_PREFIX/lib:$LD_LIBRARY_PATH"
-pip install -r requirements.txt
-# Add project files to import path
 export PYTHONPATH="$PWD/DDFA:$PYTHONPATH"
