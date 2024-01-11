bcd8e3e54c5a7edf01ddb7fb26d3fc7fa2d83565
bstee615 <benjaminjsteenhoek@gmail.com>
2024-01-10 21:25:53 -0600

    Fix download scripts to use curl (compatible with Docker) and only use self-hosted figshare links

--- scripts/download_all.sh
+++ scripts/download_all.sh
@@ -2,18 +2,17 @@
 set -e
 
 # Raw Big-Vul dataset
-gdown 'https://drive.google.com/uc?id=1-0VhnHBp9IGh90s2wCNjeCMuy70HPl8X' -O DDFA/storage/external/MSR_data_cleaned.zip
-unzip DDFA/storage/external/MSR_data_cleaned.zip -d DDFA/storage/external/
+curl -Lo MSR_data_cleaned.zip 'https://figshare.com/ndownloader/files/43990908'
+unzip MSR_data_cleaned.zip -d DDFA/storage/external/
 
 # LineVul version of Big-Vul dataset
-gdown 'https://drive.google.com/uc?id=1h0iFJbc5DGXCXXvvR6dru_Dms_b2zW4V' -O LineVul/data/MSR/test.csv
-gdown 'https://drive.google.com/uc?id=1ldXyFvHG41VMrm260cK_JEPYqeb6e6Yw' -O LineVul/data/MSR/train.csv
-gdown 'https://drive.google.com/uc?id=1yggncqivMcP0tzbh8-8Eu02Edwcs44WZ' -O LineVul/data/MSR/val.csv
+curl -Lo MSR_LineVul.zip 'https://figshare.com/ndownloader/files/43991823'
+unzip MSR_LineVul.zip -d LineVul/data/MSR
 
 # DeepDFA preprocessed data
-wget 'https://figshare.com/ndownloader/files/43917390' -O preprocessed_data.zip
+curl -Lo preprocessed_data.zip 'https://figshare.com/ndownloader/files/43991910'
 unzip preprocessed_data.zip
 
 # DeepDFA CFGs
-wget 'https://figshare.com/ndownloader/files/43916550' -O before.zip
+curl -Lo before.zip 'https://figshare.com/ndownloader/files/43916550'
 unzip before.zip -d DDFA/storage/processed/bigvul
